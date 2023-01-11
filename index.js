9d4c0c5185ee9278d854f54863e0b2f3ac530c0d
Muhammad Bilal <bilaliqbal78692110@gmail.com>
2023-01-11 23:56:54 +0500

    Add , Remove file in node cmd complete

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const fs = require("fs");
 
 const input = process.argv;
 
-fs.writeFileSync(input[2], input[3]);
-
-// fs.writeFileSync("file.txt", "Hi how are you this file is make using node js");
+// fs.writeFileSync(input[2], input[3]);
 
 // console.log(process.argv[2]); //cmd node index.js something //*It will print something
+
+if (input[2] == "add") {
+  fs.writeFileSync(input[3], input[4]);
+} else if (input[2] == "remove") {
+  fs.unlinkSync(input[3]);
+} else {
+  console.log("Invalid Data");
+}
