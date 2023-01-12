16c28b33ff0bf16cb314266f397ca28158a6ec72
Muhammad Bilal <bilaliqbal78692110@gmail.com>
2023-01-12 13:20:29 +0500

    Create 5 files with the help of for loop in nodejs cmd line

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const fs = require("fs");
+const path = require("path");
+const dirName = path.join(__dirname, "files");
 
-const input = process.argv;
+console.log(dirName);
+
+for (i = 0; i < 5; i++) {
+  fs.writeFileSync(`${i}.txt`, `This is file ${i}`);
+}
+
+// const input = process.argv;
 
 // fs.writeFileSync(input[2], input[3]);
 
 // console.log(process.argv[2]); //cmd node index.js something //*It will print something
 
-if (input[2] == "add") {
-  fs.writeFileSync(input[3], input[4]);
-} else if (input[2] == "remove") {
-  fs.unlinkSync(input[3]);
-} else {
-  console.log("Invalid Data");
-}
+// if (input[2] == "add") {
+//   fs.writeFileSync(input[3], input[4]);
+// } else if (input[2] == "remove") {
+//   fs.unlinkSync(input[3]);
+// } else {
+//   console.log("Invalid Data");
+// }
